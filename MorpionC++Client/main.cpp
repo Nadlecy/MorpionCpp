423cfa0f743da3145d7c9b85a7f646e86b78df12
Nadlecy <bruno.cleymand@mail.com>
2024-02-05 18:28:49 +0100

    0.10

---
+++ MorpionC++Client/main.cpp
@@ -0,0 +1,63 @@
+#pragma comment(lib, "ws2_32.lib")
+
+#include <winsock2.h>
+#include <windows.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
+#include <stdio.h>
+
+#define PORT 6969
+//#define IPADDRESS 10.1.144.26
+
+int main(int argc, char const* argv[])
+{
+	WORD wVersionRequested;
+	WSADATA wsaData;
+	int err;
+
+	/* Use the MAKEWORD(lowbyte, highbyte) macro declared in Windef.h */
+	wVersionRequested = MAKEWORD(2, 2);
+
+	err = WSAStartup(wVersionRequested, &wsaData);
+	if (err != 0) {
+		/* Tell the user that we could not find a usable */
+		/* Winsock DLL.                                  */
+		printf("WSAStartup failed with error: %d\n", err);
+		return 1;
+	}
+
+
+	int status, valread, client_fd;
+	struct sockaddr_in serv_addr;
+	char buffer[1024] = { 0 };
+	if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("\n Socket creation error \n");
+		return -1;
+	}
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(PORT);
+
+	// Convert IPv4 and IPv6 addresses from text to binary
+	// form
+	if (inet_pton(AF_INET, "10.1.144.26", &serv_addr.sin_addr)
+		<= 0) {
+		printf(
+			"\nInvalid address/ Address not supported \n");
+		return -1;
+	}
+
+	if ((status = connect(client_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) < 0) {
+		printf("\nConnection Failed \n");
+		return -1;
+	}
+	send(client_fd, "PLACEHOLDER MESSAGE", strlen("PLACEHOLDER MESSAGE"), 0);
+	printf("Hello message sent\n");
+	valread = recv(client_fd, buffer, 1024 - 1, 0); // subtract 1 for the null
+	// terminator at the end
+	printf("%s\n", buffer);
+
+	// closing the connected socket
+	closesocket(client_fd);
+	return 0;
+}
---
+++ MorpionC++Serveur/main.cpp
@@ -0,0 +1,83 @@
+#pragma comment(lib, "ws2_32.lib")
+
+#include <winsock2.h>
+#include <windows.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define PORT 6969
+
+using namespace std;
+
+int main(int argc, char const* argv[]) {
+    int server_fd, new_socket;
+    size_t valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    socklen_t addrlen = sizeof(address);
+    char buffer[1024] = { 0 };
+
+    WORD wVersionRequested;
+    WSADATA wsaData;
+    int err;
+
+    /* Use the MAKEWORD(lowbyte, highbyte) macro declared in Windef.h */
+    wVersionRequested = MAKEWORD(2, 2);
+
+    err = WSAStartup(wVersionRequested, &wsaData);
+    if (err != 0) {
+        /* Tell the user that we could not find a usable */
+        /* Winsock DLL.                                  */
+        printf("WSAStartup failed with error: %d\n", err);
+        return 1;
+    }
+
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Forcefully attaching socket to the port 6969
+    if (setsockopt(server_fd, SOL_SOCKET,
+        SO_REUSEADDR, (char*) &opt,
+        sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // Forcefully attaching socket to the port 6969
+    if (bind(server_fd, (struct sockaddr*)&address,
+        sizeof(address))
+        < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket = accept(server_fd, (struct sockaddr*)&address, &addrlen)) < 0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    valread = recv(new_socket, buffer, 1024 - 1, 0); // subtract 1 for the null
+    // terminator at the end
+    printf("%s\n", buffer);
+    send(new_socket, "tu te chut toi", strlen("tu te chut toi"), 0);
+    printf("Hello message sent\n");
+
+    // closing the connected socket
+    closesocket(new_socket);
+
+    // closing the listening socket
+    closesocket(server_fd);
+    return 0;
+}
